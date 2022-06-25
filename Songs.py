673e3a45f288fac511e936548d3c503c0134a268
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-06-25 17:47:10 +0530

    Update Songs.py

--- Songs.py
+++ Songs.py
@@ -1,4 +1,14 @@
-import lastfm
-from requests import auth
+import requests
 
-auth.getSession()
+headers = {
+    'user-agent': 'Udayan Pandey'
+}
+
+payload = {
+    'api_key': '7921b4b4bfa5aa3d685c70de8a3eefc6',
+    'method': 'chart.gettopartists',
+    'format': 'json'
+}
+
+r = requests.get('https://ws.audioscrobbler.com/2.0/', headers=headers, params=payload)
+print(r.status_code)
