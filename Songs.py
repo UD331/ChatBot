4174c67a8622b9eb99f3a3fc6f35902c1f7cda04
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-06-29 11:23:12 +0530

    Update Songs.py

--- Songs.py
+++ Songs.py
@@ -24,3 +24,43 @@ def jprint(obj):
 
 jprint(r.json())
 jprint(r.json()['artists']['@attr'])
+
+import time
+
+responses = []
+
+page = 1
+total_pages = 99999 # this is just a dummy number so the loop starts
+
+while page <= total_pages:
+    payload = {
+        'method': 'chart.gettopartists',
+        'limit': 500,
+        'page': page
+    }
+
+    # print some output so we can see the status
+    print("Requesting page {}/{}".format(page, total_pages))
+    # clear the output to make things neater
+
+    # make the API call
+    response = lastfm_get(payload)
+
+    # if we get an error, print the response and halt the loop
+    if response.status_code != 200:
+        print(response.text)
+        break
+
+    # extract pagination info
+    page = int(response.json()['artists']['@attr']['page'])
+    total_pages = int(response.json()['artists']['@attr']['totalPages'])
+
+    # append response
+    responses.append(response)
+
+    # if it's not a cached result, sleep
+    if not getattr(response, 'from_cache', False):
+        time.sleep(0.25)
+
+    # increment the page number
+    page += 1
