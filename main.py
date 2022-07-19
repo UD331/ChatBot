23946d9d90d0dbc9b44f65f937d46550cc248715
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-07-19 10:49:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import Word_Analysis
 import Songs
 
-s = input("Please enter words")
+s = input("Please enter words\n")
 
 analysis = Word_Analysis.interpreter(s)
+print(analysis)
 score = 2
 feeling = ""
-
+#Select highest score
 for i in analysis:
     if analysis.get(i) < score:
         score = analysis.get(i)
         feeling = i
 
 payload = Songs.get_payload(feeling)
-
+print(payload)
 obj = Songs.jprint(payload)
-
+print(obj)
 Songs.getSong(obj)
-
 
 
